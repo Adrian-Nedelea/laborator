886b14b6f5642f22808f0956ddd6e0ce5a794a4a
Adrian-Nedelea <84146769+Adrian-Nedelea@users.noreply.github.com>
2021-10-17 17:28:03 +0300

    Add files via upload

---
+++ Nedelea Adrian/tema2/Tema1DATC/Controllers/StudentController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Models;
+using Repositories;
+
+namespace Tema1DATC.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentController : ControllerBase
+    {
+        public StudentController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return StudentRepo.Students;
+        }
+    }
+}
---
+++ Nedelea Adrian/tema2/Tema1DATC/Models/student.cs
@@ -0,0 +1,13 @@
+namespace Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Faculty { get; set; }
+
+        public int Year { set; get; }
+    }
+}
