cb3061587d5660a367007f65d3d0cacbfeb4c420
Adrian-Nedelea <84146769+Adrian-Nedelea@users.noreply.github.com>
2021-11-14 18:50:21 +0200

    Add files via upload

---
+++ Nedelea Adrian/Tema5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace Tema5Datc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var count = 0;
+            while (true)
+            {
+                Console.WriteLine("Cycle no." + count.ToString());
+                Console.WriteLine("Date: " + DateTime.Now.ToLocalTime());
+                count++;
+                Thread.Sleep(2000);
+            }
+        }
+    }
+}
